ecc0bd8f1c7bcec963cd3144b0331c36c8c6328d
Nofre Móra <nodiek@gmail.com>
2010-07-06 18:47:52 +0200

    G-Keys

--- g15manager.py
+++ g15manager.py
@@ -5,7 +5,9 @@ import applets.emesene
 import applets.top
 import applets.gmail
 import gtk
+import keybinder
 import os
+
 
 class Main:
     def __init__(self):
@@ -13,6 +15,9 @@ class Main:
         builder.add_from_file("gui.glade")
         builder.connect_signals(self)
 
+        import gkeys
+
+        self.window = builder.get_object("window")
         self.check_amarok = builder.get_object("check_amarok")
         self.check_top = builder.get_object("check_top")
         self.check_emesene = builder.get_object("check_emesene")
@@ -20,7 +25,40 @@ class Main:
         self.check_gmail = builder.get_object("check_gmail")
         self.gmail_user = builder.get_object("gmail_user")
         self.gmail_passwd = builder.get_object("gmail_passwd")
-        self.g15stats_interface = builder.get_object("g15stats_interface")
+        self.g15stats_interface = builder.get_object("g15stats_interface")     
+        self.popup_menu = builder.get_object("popup_menu")
+
+        self.command_g1 = builder.get_object("command_g1")
+        self.command_g2 = builder.get_object("command_g2")
+        self.command_g3 = builder.get_object("command_g3")
+        self.command_g4 = builder.get_object("command_g4")
+        self.command_g5 = builder.get_object("command_g5")
+        self.command_g6 = builder.get_object("command_g6")
+        self.command_g7 = builder.get_object("command_g7")
+        self.command_g8 = builder.get_object("command_g8")
+        self.command_g9 = builder.get_object("command_g9")
+        self.command_g10 = builder.get_object("command_g10")
+        self.command_g11 = builder.get_object("command_g11")
+        self.command_g12 = builder.get_object("command_g12")
+        self.command_g13 = builder.get_object("command_g13")
+        self.command_g14 = builder.get_object("command_g14")
+        self.command_g15 = builder.get_object("command_g15")
+        self.command_g16 = builder.get_object("command_g16")
+        self.command_g17 = builder.get_object("command_g17")
+        self.command_g18 = builder.get_object("command_g18")
+
+#        import appindicator
+#        indicator = appindicator.Indicator("g15manager",
+#                "distributor-logo", appindicator.CATEGORY_APPLICATION_STATUS)
+#        indicator.set_status (appindicator.STATUS_ACTIVE)
+#        indicator.set_menu(self.popup_menu)
+
+        self.statusicon = gtk.StatusIcon()
+        self.statusicon.set_from_file("g15.png")
+        self.statusicon.connect("popup-menu", self.statusicon_right_click)
+        self.statusicon.connect("activate", self.statusicon_left_click)
+        self.statusicon.set_visible(True)
+
 
         self.processes = [0, 0, 0, 0, 0]
 
@@ -48,11 +86,33 @@ class Main:
                 self.config.write(configfile)
 
 
-    def window_destroy(self,widget):
+        keys = ["G1","G2","G3","G4","G5","G6","G7","G8","G9","G10","G11","G12","G13","G14","G15","G16","G17","G18"]
+
+
+        for key in keys:
+            keybinder.bind(key, self.key_pressed, key)
+
+
+    def statusicon_left_click(self, widget):
+        if self.window.is_active() == True:
+            self.window.hide()
+        else:
+            self.window.present()
+
+    def statusicon_right_click(self,icon,button,time):
+        self.popup_menu.popup(None, None, gtk.status_icon_position_menu, button, time, self.statusicon)
+
+
+    def quit(self, widget):
         for i in self.processes:
             if i != 0:
                 i.kill()
         gtk.main_quit()
+
+
+    def window_hide(self,widget,data=None):
+        self.window.hide()
+        return True
 
 
     def check_g15stats_toggled(self, widget):
@@ -118,6 +178,43 @@ class Main:
         with open("%s/.g15manager" % os.environ["HOME"], "w") as configfile:
                 self.config.write(configfile)
 
+    def key_pressed(self, key):
+        if key == "G1":
+            subprocess.Popen(self.command_g1.get_text())
+        elif key == "G2":
+            subprocess.Popen(self.command_g2.get_text())
+        elif key == "G3":
+            subprocess.Popen(self.command_g3.get_text())
+        elif key == "G4":
+            subprocess.Popen(self.command_g4.get_text())
+        elif key == "G5":
+            subprocess.Popen(self.command_g5.get_text())
+        elif key == "G6":
+            subprocess.Popen(self.command_g6.get_text())
+        elif key == "G7":
+            subprocess.Popen(self.command_g7.get_text())
+        elif key == "G8":
+            subprocess.Popen(self.command_g8.get_text())
+        elif key == "G9":
+            subprocess.Popen(self.command_g9.get_text())
+        elif key == "G10":
+            subprocess.Popen(self.command_g10.get_text())
+        elif key == "G11":
+            subprocess.Popen(self.command_g11.get_text())
+        elif key == "G12":
+            subprocess.Popen(self.command_g12.get_text())
+        elif key == "G13":
+            subprocess.Popen(self.command_g13.get_text())
+        elif key == "G14":
+            subprocess.Popen(self.command_g14.get_text())
+        elif key == "G15":
+            subprocess.Popen(self.command_g15.get_text())
+        elif key == "G16":
+            subprocess.Popen(self.command_g16.get_text())
+        elif key == "G17":
+            subprocess.Popen(self.command_g17.get_text())
+        elif key == "G18":
+            subprocess.Popen(self.command_g18.get_text())
 
 
 if __name__ == "__main__":
